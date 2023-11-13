43bb8aaed783e0b613a20e3aaaeed61399e73b43
Olga Malashenko <olga_birjukova@mail.ru>
2023-11-14 00:53:16 +0300

    inheritance bug fixed

--- src/js/app.js
+++ src/js/app.js
@@ -5,67 +5,88 @@ import sum from './basic';
 // console.log(sum([1, 2]));
 
 export class Character {
-  constructor(name, type) {
-    try {
-      if (typeof (name) === 'string' && name.length > 2 && name.length < 10) {
-        this.name = name;
-      } else {
-        console.log('Имя не соответствует критериям');
-      }
-    } catch (err) {
-      throw new Error('Имя не соответствует критериям');
-    }
-
-    try {
-      const typeList = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
-      let str = false;
-      for (const element of typeList) {
-        if (type === element) {
-          str = true;
-          break;
+    constructor(name, type) {
+      try {
+        if (typeof (name) === 'string' && name.length > 2 && name.length < 10) {
+          this.name = name;
+        } else {
+          console.log('Имя не соответствует критериям');
         }
+      } catch (err) {
+        throw new Error('Имя не соответствует критериям');
       }
-      if (typeof (type) === 'string' && str) {
-        this.type = type;
-      } else {
-        console.log('Неизвестный тип');
+  
+      try {
+        const typeList = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
+        let str = false;
+        for (const element of typeList) {
+          if (type === element) {
+            str = true;
+            break;
+          }
+        }
+        if (typeof (type) === 'string' && str) {
+          this.type = type;
+        } else {
+          console.log('Неизвестный тип');
+        }
+      } catch (err) {
+        throw new Error('Неизвестный тип');
       }
-    } catch (err) {
-      throw new Error('Неизвестный тип');
+  
+      this.health = 100;
+      this.level = 1;
+  
+      this.attack = 1;
+      this.defence = 1;
     }
-
-    this.health = 100;
-    this.level = 1;
-
-    let attack;
-    let defence;
-    switch (type) {
-      case 'Bowman':
-        attack = 25;
-        defence = 25;
-        break;
-      case 'Swordsman':
-        attack = 40;
-        defence = 10;
-        break;
-      case 'Magician':
-        attack = 10;
-        defence = 40;
-        break;
-      case 'Undead':
-        attack = 25;
-        defence = 25;
-        break;
-      case 'Zombie':
-        attack = 40;
-        defence = 10;
-        break;
-      case 'Daemon':
-        attack = 10;
-        defence = 40;
-        break;
-    }
-    this.attack = attack;
-    this.defence = defence;
   }
-}
+  
+  export class Bowman extends Character {
+    constructor(name, type) {
+      super(name, type);
+      this.attack = 25;
+      this.defence = 25;
+    }
+  }
+  
+  export class Swordsman extends Character {
+    constructor(name, type) {
+      super(name, type);
+      this.attack = 40;
+      this.defence = 10;
+    }
+  }
+  
+  export class Magician extends Character {
+    constructor(name, type) {
+      super(name, type);
+      this.attack = 10;
+      this.defence = 40;
+    }
+  }
+  
+  export class Undead extends Character {
+    constructor(name, type) {
+      super(name, type);
+      this.attack = 25;
+      this.defence = 25;
+    }
+  }
+  
+  export class Zombie extends Character {
+    constructor(name, type) {
+      super(name, type);
+      this.attack = 40;
+      this.defence = 10;
+    }
+  }
+  
+  export class Daemon extends Character {
+    constructor(name, type) {
+      super(name, type);
+      this.attack = 10;
+      this.defence = 40;
+    }
+  }
+  
